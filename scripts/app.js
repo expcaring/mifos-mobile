9828d35da4f10a6076be394a3efe1229abaee932
Andrew Pickard <apickard@expedia.com>
2012-09-07 10:23:38 -0700

    added app.js

---
+++ scripts/app.js
@@ -0,0 +1,19 @@
+var mifos = {}
+mifos.api.url = "https://ec2-46-137-62-163.eu-west-1.compute.amazonaws.com:8443/";
+mifos.login = function(username, password)
+{
+var jqxhr = $.ajax({
+        url : mifos.api.url + "authentication?username=" + username + "&password=" + password,
+        type : 'POST',
+        contentType : "application/json; charset=utf-8",
+        dataType : 'json',
+        data : "{}",
+        cache : false,
+        success : function(data, textStatus, jqXHR) {     
+            mifos.basicAuthKey = data.base64EncodedAuthenticationKey;
+        },
+        error : function(jqXHR, textStatus, errorThrown) {
+            //
+        }
+    });
+}
