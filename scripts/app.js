973b22ed46c4a36c63f86ea9569f067708505b84
Andrew Pickard <apickard@expedia.com>
2012-09-07 11:10:41 -0700

    added user list and user details ajax request

--- scripts/app.js
+++ scripts/app.js
@@ -1,7 +1,8 @@
 var mifos = {}
 mifos.api.url = "https://ec2-46-137-62-163.eu-west-1.compute.amazonaws.com:8443/";
 mifos.api.tenantId = "&tenantIdentifier=default"
-mifos.login = function(username, password)
+mifos.api.users = "api/v1/users"
+mifos.api.login = function(username, password)
 {
 var jqxhr = $.ajax({
         url : mifos.api.url + "authentication?username=" + username + "&password=" + password + mifos.api.tenantId,
@@ -11,10 +12,60 @@ var jqxhr = $.ajax({
         data : "{}",
         cache : false,
         success : function(data, textStatus, jqXHR) {     
-            mifos.basicAuthKey = data.base64EncodedAuthenticationKey;
+            mifos.api.basicAuthKey = data.base64EncodedAuthenticationKey;
         },
         error : function(jqXHR, textStatus, errorThrown) {
             //
         }
     });
+}
+
+mifos.api.executeAjaxRequest = function(request, requestType, inputData, successFunction, errorFunction)
+{
+	var jqxhr = $.ajax({
+        url : mifos.api.url +  request + mifos.api.tenantId,
+        type : requestType, //POST, GET, PUT or DELETE
+        contentType : "application/json; charset=utf-8",
+        dataType : 'json',
+        data : inputData, //As JSON
+        cache : false,
+        beforeSend : function(xhr) {
+                xhr.setRequestHeader("Authorization", "Basic " + mifos.api.basicAuthKey);
+        },
+        success : successFunction(xhr),
+        error : errorFunction(xhr)
+    });
+}
+
+mifos.api.listUsers = function(fields)
+{
+	var request = fields ? mifos.api.users + "?" + "fields=" + fields : mifos.api.users;
+	mifos.api.executeAjaxRequest(request, "GET", {}, mifos.api.listUsersSuccess, mifos.api.listUsersError);
+}
+
+mifos.api.listUsersSuccess = function(jsonResponse)
+{
+	//TODO
+}
+
+mifos.api.listUsersError = function(jsonResponse)
+{
+	//TODO
+}
+
+mifos.api.userDetails = function(id, fields)
+{
+	var request = mifos.api.users + "/" + userId + "?fields=" + fields;
+	mifos.api.executeAjaxRequest = function(request, "GET", {}, mifos.api.userDetailsSuccess, mifos.api.userDetailsError);
+}
+
+
+mifos.api.userDetailsSuccess = function(jsonResponse)
+{
+	//TODO
+}
+
+mifos.api.userDetailsError = function(jsonResponse)
+{
+	//TODO
 }
