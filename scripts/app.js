86ef0b909a78786b9a0dde863b46b18fb55e0447
Andrew Pickard <apickard@expedia.com>
2012-09-07 12:48:31 -0700

    all sorts of stuffs

--- scripts/app.js
+++ scripts/app.js
@@ -1,87 +1,107 @@
-var mifos = {}
-mifos.api.url = "https://ec2-46-137-62-163.eu-west-1.compute.amazonaws.com:8443/";
-mifos.api.tenantId = "&tenantIdentifier=default"
-mifos.api.users = "api/v1/users"
-mifos.api.login = function(username, password)
+var mifos = {};
+mifos.url = "https://ec2-46-137-62-163.eu-west-1.compute.amazonaws.com:8443/";
+mifos.tenantId = "&tenantIdentifier=default"
+mifos.users = "api/v1/users"
+mifos.login = function(username, password)
 {
 var jqxhr = $.ajax({
-        url : mifos.api.url + "authentication?username=" + username + "&password=" + password,
+        url : mifos.url + "authentication?username=" + username + "&password=" + password,
         type : 'POST',
         contentType : "application/json; charset=utf-8",
         dataType : 'json',
         data : "{}",
         cache : false,
         success : function(data, textStatus, jqXHR) {     
-            mifos.api.basicAuthKey = data.base64EncodedAuthenticationKey;
-            if(data.error)
+            mifos.basicAuthKey = data.base64EncodedAuthenticationKey;
+            if(data)
             {
-                //Show error message
-                var errorMessage = data.error[0].defaultUserMessage;
-                mifos.api.loginFailed(errorMessage);
+                if(data.errors)
+                {
+                    //Show error message
+                    mifos.loginFailed(data);
+                }
+                else
+                {
+                    //Load main menu
+                    
+                }
             }
-            //Load main menu
         },
         error : function(jqXHR, textStatus, errorThrown) {
             //Show error message
-            mifos.api.login.failed("loginFailed");
-
+            mifos.login.failed({errors : [{defaultUserMessage : "loginFailed"}]});
         }
     });
 }
 
-mifos.api.loginFailed(error)
+mifos.loginFailed = function(data)
 {
     var source   = $("#signIn-template").html();
     var template = Handlebars.compile(source);
-    var html = template(error);
+    var html;
+    if(data.errors)
+     {
+        html = template(data);
+     }
+     else
+     {
+        html = template({errors : []});
+     }
     $("mainContent").append(html);
 }
 
-mifos.api.executeAjaxRequest = function(request, requestType, inputData, successFunction, errorFunction)
+mifos.executeAjaxRequest = function(request, requestType, inputData, successFunction, errorFunction)
 {
 	var jqxhr = $.ajax({
-        url : mifos.api.url +  request + mifos.api.tenantId,
+        url : mifos.url +  request + mifos.tenantId,
         type : requestType, //POST, GET, PUT or DELETE
         contentType : "application/json; charset=utf-8",
         dataType : 'json',
         data : inputData, //As JSON
         cache : false,
         beforeSend : function(xhr) {
-                xhr.setRequestHeader("Authorization", "Basic " + mifos.api.basicAuthKey);
+                xhr.setRequestHeader("Authorization", "Basic " + mifos.basicAuthKey);
         },
         success : successFunction(xhr),
         error : errorFunction(xhr)
     });
 }
 
-mifos.api.listUsers = function(fields)
+mifos.listUsers = function(fields)
 {
-	var request = fields ? mifos.api.users + "?" + "fields=" + fields : mifos.api.users;
-	mifos.api.executeAjaxRequest(request, "GET", {}, mifos.api.listUsersSuccess, mifos.api.listUsersError);
+	var request = fields ? mifos.users + "?" + "fields=" + fields : mifos.users;
+	mifos.executeAjaxRequest(request, "GET", {}, mifos.listUsersSuccess, mifos.listUsersError);
 }
 
-mifos.api.listUsersSuccess = function(jsonResponse)
+mifos.listUsersSuccess = function(jsonResponse)
 {
 	//TODO
 }
 
-mifos.api.listUsersError = function(jsonResponse)
+mifos.listUsersError = function(jsonResponse)
 {
 	//TODO
 }
 
-mifos.api.userDetails = function(id, fields)
+mifos.userDetails = function(id, fields)
 {
-	var request = mifos.api.users + "/" + userId + "?fields=" + fields;
-	mifos.api.executeAjaxRequest = function(request, "GET", {}, mifos.api.userDetailsSuccess, mifos.api.userDetailsError);
+	var request = mifos.users + "/" + userId + "?fields=" + fields;
+	mifos.executeAjaxRequest(request, "GET", {}, mifos.userDetailsSuccess, mifos.userDetailsError);
 }
 
-mifos.api.userDetailsSuccess = function(jsonResponse)
+mifos.userDetailsSuccess = function(jsonResponse)
 {
 	//TODO
 }
 
-mifos.api.userDetailsError = function(jsonResponse)
+mifos.userDetailsError = function(jsonResponse)
 {
 	//TODO
 }
+
+$(document).ready(function(){
+    var source   = $("#signIn-template").html();
+    var template = Handlebars.compile(source);
+    var html = template({errors : []});
+    $("mainContent").append(html);
+});
