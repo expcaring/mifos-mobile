ab265e5d615226f8801d29048c02d7679a753880
Andrew Pickard <apickard@expedia.com>
2012-09-07 10:27:25 -0700

    updated login js

--- scripts/app.js
+++ scripts/app.js
@@ -1,9 +1,10 @@
 var mifos = {}
 mifos.api.url = "https://ec2-46-137-62-163.eu-west-1.compute.amazonaws.com:8443/";
+mifos.api.tenantId = "&tenantIdentifier=default"
 mifos.login = function(username, password)
 {
 var jqxhr = $.ajax({
-        url : mifos.api.url + "authentication?username=" + username + "&password=" + password,
+        url : mifos.api.url + "authentication?username=" + username + "&password=" + password + mifos.api.tenantId,
         type : 'POST',
         contentType : "application/json; charset=utf-8",
         dataType : 'json',
